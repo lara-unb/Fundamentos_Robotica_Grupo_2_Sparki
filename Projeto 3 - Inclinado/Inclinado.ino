5d20c5278f7d97ca712c490b1519188c4c72aaa0
ItaloCavalcant <130604394+ItaloCavalcant@users.noreply.github.com>
2023-06-06 16:37:05 -0300

    Update Inclinado.ino
    Mudança no código

--- Projeto 3 - Inclinado/Inclinado.ino
+++ Projeto 3 - Inclinado/Inclinado.ino
@@ -2,7 +2,6 @@
  
 void setup()
 {
- 
 }
  
 void loop()
@@ -11,27 +10,28 @@ void loop()
     float y = sparki.accelY();
  
     // is Sparki already facing downhill or uphill?
-    if ( (x < 1) && (x > -1) )
+    if ( (x < 0.75) && (x > -0.75) )
     {
-      if ( y < -1 ) // if sparki is facing uphill
+      if ( y < -0.75 ) // if sparki is facing uphill
       {
         sparki.moveForward();
+        delay(650);
       }
-      if ( y > 1 ) // if sparki is facing downhill
+      if ( y > 1.25 ) // if sparki is facing downhill
       {
-        sparki.moveBackward();
+        sparki.moveRight();
       } 
-      if (-0.5 < y && y < 0.5)
+      if (-0.75 < y && y < 1.25)
       {
         sparki.moveStop();
       }
     }
     else{ // if Sparki is not facing uphill or downhill, have Sparki turn until it is
-      if ( x > 1 ) // if tilted right
+      if ( x > 0.75 ) // if tilted right
       {
         sparki.moveLeft();
       }
-      if ( x < -1 ) // if tilted left
+      if ( x < -0.75 ) // if tilted left
       {
         sparki.moveRight();
       }   
